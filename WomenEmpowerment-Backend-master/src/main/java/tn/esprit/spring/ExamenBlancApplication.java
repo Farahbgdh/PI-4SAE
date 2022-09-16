a2d28cc8352c3e4cdd8b60b56b75f8844e2f177a
Farahbgdh <79215597+Farahbgdh@users.noreply.github.com>
2022-09-16 17:01:20 +0200

    Last commit

---
+++ WomenEmpowerment-Backend-master/src/main/java/tn/esprit/spring/ExamenBlancApplication.java
@@ -0,0 +1,22 @@
+package tn.esprit.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableAspectJAutoProxy
+@EnableScheduling
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableSwagger2
+public class ExamenBlancApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenBlancApplication.class, args);
+	}
+
+}
